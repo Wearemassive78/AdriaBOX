5fcf27a0c843a9c7d6ea60b5a0e2d10aa345b037
Sajmir <sajmir.buzi@studio.unibo.it>
2026-04-13 16:20:06 +0200

    added folder test and test files for first impl

--- src/metadata_server/__init__.py
+++ src/metadata_server/__init__.py
@@ -1,1 +1,9 @@
-# Metadata server package
+import json
+
+def store_metadata(metadata):
+	with open('metadata.json', 'w') as f:
+		f.write(json.dumps(metadata))
+
+def retrieve_metadata(file_id):
+	with open('metadata.json', 'r') as f:
+		return json.loads(f.read())
